83df10a79428bf2b5347e311b1bcaa8e6484f4ed
Pratyush Rao <pratyush.rao@theopeneyes.in>
2025-04-15 21:09:30 +0530

    Adding the initial sb app

---
+++ math-app/src/main/java/com/mathnimation/math_app/MathAppApplication.java
@@ -0,0 +1,13 @@
+package com.mathnimation.math_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathAppApplication.class, args);
+	}
+
+}
---
+++ math-app/src/test/java/com/mathnimation/math_app/MathAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathnimation.math_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
