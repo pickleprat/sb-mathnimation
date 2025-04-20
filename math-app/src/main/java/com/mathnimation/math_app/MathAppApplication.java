d902dceec1390b0e540c870c3472c419561d0bb8
pickleprat <prat16here@gmail.com>
2025-04-20 10:39:03 +0530

    removed unnecessary code

--- math-app/src/main/java/com/mathnimation/math_app/MathAppApplication.java
+++ math-app/src/main/java/com/mathnimation/math_app/MathAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MathAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting springboot application....");
 		SpringApplication.run(MathAppApplication.class, args);
 	}
 
